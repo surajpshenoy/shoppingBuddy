032267f73fdedfeda6af14e3f30e9d028178be33
surajpshenoy <surajpshenoy@gmail.com>
2021-08-24 16:56:08 +1000

    Input field added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React, { useState } from "react";
 import Alert from "./Alert";
-import List from "./List"
+import List from "./List";
 
 function App() {
   const [name, setName] = useState();
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
-  const [alert, setAlert] = useState({ show: true, msg: "", type: "" });
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Clicked');
-  }
+    console.log("Clicked");
+  };
 
   return (
     <section className="section-center">
-      <form classname="grocery-form" onSubmit={handleSubmit}>
-      {alert.show && <Alert/>}
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Shopping Buddy</h3>
+        <div className="form-control">
+          <input type="text" className="grocery" value={name} onChange={(e)=>{setName(e.target.value)}}placeholder="Eg: Eggs" />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
       </form>
       <div className="grocery-container">
-        <List/>
-        <button className="clear-btn">
-        Clear Items
-        </button>
+        <List />
+        <button className="clear-btn">Clear Items</button>
       </div>
-      
-
     </section>
   );
 }
