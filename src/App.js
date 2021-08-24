84b0e511532e8a7ba5f2f49b1be8ca6990de9c32
surajpshenoy <surajpshenoy@gmail.com>
2021-08-24 16:31:48 +1000

    Structure setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react"
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState();
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
   return (
-    <div className="App">
-      <h4>Hello</h4>
-    </div>
+    <section className="section-center">
+      <div className="grocery-container">
+     
+      </div>
+      
+
+    </section>
   );
 }
 
